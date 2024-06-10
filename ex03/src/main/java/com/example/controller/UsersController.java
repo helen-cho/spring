be57f2bafdd10d194f1dbf3d204f62e0cb8f6906
helen-cho <hd9536@daum.net>
2024-06-10 13:20:24 +0900

    사용자정보

--- ex03/src/main/java/com/example/controller/UsersController.java
+++ ex03/src/main/java/com/example/controller/UsersController.java
@@ -1,12 +1,25 @@
 package com.example.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.dao.UsersDAO;
 
 
 @Controller
 @RequestMapping("/users")
 public class UsersController {
+	@Autowired
+	UsersDAO dao;
+	
+	@GetMapping("/read/{uid}")
+	public String read(@PathVariable("uid") String uid, Model model) {
+		model.addAttribute("user", dao.read(uid));
+		return "/users/read.html";
+	};
 	
 	@GetMapping("/list")
 	public String list() {
