d410330df659ca027c2f76f828250cd839da65be
helen-cho <hd9536@daum.net>
2024-06-14 16:14:14 +0900

    이미지

--- ex05/src/main/java/com/example/controller/UsersRestController.java
+++ ex05/src/main/java/com/example/controller/UsersRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.dao.UserDAO;
 import com.example.domain.UserVO;
 
@@ -26,3 +27,6 @@ public class UsersRestController {
 		dao.update(vo);
 	}
 }
+
+
+
